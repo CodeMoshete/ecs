6786f8290b4bc0aa8d525b8f9cc2209f4ee3142b
Benjamin Mosher <codemoshete@gmail.com>
2020-03-08 12:59:52 -0700

    Add placeholder system

---
+++ DOTS Testing/Assets/Source/Systems/ShipSystem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+
+public class ShipSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Rotation rotation) => 
+        {
+            quaternion currentRotation = rotation.Value.;
+        });
+    }
+}
